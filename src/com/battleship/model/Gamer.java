06034394cb285f4fbee79776e604dc180e42404b
HaliDey <Hdeyali@gmail.com>
2015-01-08 00:46:24 +0100

    Ajout de commentaire dans la classe Gamer du modele

--- src/com/battleship/model/Gamer.java
+++ src/com/battleship/model/Gamer.java
@@ -13,36 +13,48 @@ import com.battleship.model.Ship;
  * Seurin Audrey
  * Zerrouq Noha
  */
-public class Gamer {
+//Declaration de la classe Gamer
 
-	private Vector<Ship> ships;
+public class Gamer {
+	
+//Declaration des attributs
+	
+	private Vector<Ship> ships;//Declaration d'un Vector <Ship> dont l'objet extensible est ici nommés ships qui selectionne tous les bateaux
 	private String name;
 	private int goal;
-	private int nb_stockes;    /*number of stockes(nombre de coups(attaques)*/
+	private int nb_stockes; //nombre de stockes (Nombre de coups (Attaques)
 	private Grid grid;
-
+	
+//Declaration du constructeur de la classe Gamer
+	
 	public Gamer(Grid grid, Vector<Ship> ships){
+	
 		this.setGrid(grid);
 		this.setShips(ships);
 	}
-
+	
+//Declaration de la methode shoot()
+	
 	public void shoot(){
-		System.out.println("[GamerModel | shoot]");
+		
+		System.out.println("[GamerModel | shoot]");//afficher le joueur et ces tirs
 		
 	}
 	
+//Declaration des getters
+	
 	public Grid getGrid() {
 		return grid;
-	}
-
-	public void setGrid(Grid grid) {
-		this.grid = grid;
-	}
-
+	}	
 	public Vector<Ship> getShips() {
 		return ships;
 	}
-
+	
+//Declaration des setters
+	
+	public void setGrid(Grid grid) {
+		this.grid = grid;
+	}
 	public void setShips(Vector<Ship> ships) {
 		this.ships = ships;
 	}
